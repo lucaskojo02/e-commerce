e5c90e4264dfdc0f7852e0713255c2df86090569
Lucas Kojo <160460135+lucaskojo02@users.noreply.github.com>
2025-02-09 06:22:15 +0300

    made the select option interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -24,7 +24,7 @@ products.forEach(product=>{
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -57,7 +57,9 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach(button=>{
   button.addEventListener('click',()=>{
 
-    const productId = button.getAttribute('data-product-id')
+    const productId = button.getAttribute('data-product-id');
+    let dropDownList = document.querySelector(`.js-quantity-selector-${productId}`).value;
+    dropDownList = Number(dropDownList);
     let matchingItem;
     
     cart.forEach(item=>{
@@ -67,14 +69,16 @@ document.querySelectorAll('.js-add-to-cart').forEach(button=>{
     })
 
     if (matchingItem){
-      matchingItem.quantity++
+      matchingItem.quantity+=dropDownList;
     }
     else{
       cart.push({
         productId:productId,
-        quantity:1
+        quantity:dropDownList
       })
     }
+
+    console.log(cart);
 
     let cartQuantity = 0;
 
