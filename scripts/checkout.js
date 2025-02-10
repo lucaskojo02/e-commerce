cacd4e30cc6b98cb88fde218136cfd0c671c8425
Lucas Kojo <160460135+lucaskojo02@users.noreply.github.com>
2025-02-10 16:14:55 +0300

    imported an external library,(DayJs) imported esm version of DayJS

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,7 @@
 import { products } from '../data/products.js'
 import {cart, removeFromCart, calculateCartQuantity, updateQuantity} from '../data/cart.js';
 import { formatCurrency } from './utils/money.js';
-import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js' 
-
-hello();
 
 const today = dayjs();
 const deliveryDate =today.add(7, 'days');
