18679bb2fbf7d06ed6f20a303643c72e554e3d22
Lucas Kojo <160460135+lucaskojo02@users.noreply.github.com>
2025-02-10 09:20:12 +0300

    imported esm version of DayJS

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,14 @@
 import { products } from '../data/products.js'
 import {cart, removeFromCart, calculateCartQuantity, updateQuantity} from '../data/cart.js';
 import { formatCurrency } from './utils/money.js';
+import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js' 
+
+hello();
+
+const today = dayjs();
+const deliveryDate =today.add(7, 'days');
+console.log(deliveryDate.format('dddd,MMMM D'))
 
 let summaryHTML = '';
 
